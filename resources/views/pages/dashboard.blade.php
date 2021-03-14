f33111dc87d0a61dbf663e02368c133a86dc6b2a
RianIslam <rianislamdr@gmail.com>
2021-03-14 20:30:11 +0600

    dashboard change

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Static Navigation - SB Admin</title>
-        <link href="{{('css/dashboard.css')}}" rel="stylesheet" />
+        <title>Dashboard</title>
+        <link href="{{asset('css/dashboard.css')}}" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body>
@@ -145,6 +145,6 @@
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{assets('js/dashboard.js')}}"></script>
+        <script src="{{asset('js/dashboard.js')}}"></script>
     </body>
 </html>
